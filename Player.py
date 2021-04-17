7ed01c3f56d92b00d2024e18f84f5b0245b77461
AndresILopez <ailopez@miuandes.cl>
2021-04-17 16:46:07 -0400

    actualizando la clase Player

--- Player.py
+++ Player.py
@@ -4,5 +4,23 @@
 """
 
 class Player:
-    def __init__:
-        
+    def __init__(self, name, influence, coins):
+        self.__name = name
+        self.__influence = influence
+        self.__coins = coins
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, value):
+        self.__name = value
+    
+    @property
+    def influence(self):
+        return self.__influence
+    
+    @influence.setter
+    def influence(self, value):
+        self.__influence = value
