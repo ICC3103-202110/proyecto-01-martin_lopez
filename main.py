c08a27e01d8eebb188e60ae516088313c26fe0c5
AndresILopez <ailopez@miuandes.cl>
2021-04-18 17:10:38 -0400

    actualizando el main

--- main.py
+++ main.py
@@ -3,20 +3,30 @@
 
 """
 
-from Player.py import Player
+from Player import Player
 
+players = []
 influence_deck = []
+
+def create_player():
+    name = input("ingresa el nombre del jugador ")
+    coins = 2
+    #falta que el jugador reciba dos influencias de influence_deck
+    players.append(Player(name, influence, coins))
 
 def print_menu_and_select():
     print ("\nSelecciona una opción:")
+    print ("1. Ingresar un jugador")
     print ("0. Salir")
     return int(input())
     
-def menu():
+def inicial_menu():
     while True:
         selection = print_menu_and_select()
         if selection == 0:
             break
+        if selection == 1:
+            create_player()
 
 if __name__ == "__main__":
-    menu()
+    inicial_menu()
