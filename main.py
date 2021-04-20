0c433c0acb18839a121b6f90261968cc254aed70
Sebastian Martin Muñoz <sebamm1999@gmail.com>
2021-04-20 17:52:29 -0400

    Comienzo desarrollo del juego con situaciones y acciones planteadas

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from numpy import random
 from Player import Player
 
 players = []
-def shuffle_deck():
+def shuffle_deck(): #baraja las cartas de manera aleatoria
     deck = ["Duque", "Duque", "Duque", "Asesino", "Asesino", "Asesino", 
     "Capitán", "Capitán", "Capitán", "Embajador", "Embajador", "Embajador", 
     "Condesa", "Condesa", "Condesa"]
@@ -19,40 +19,73 @@ def create_player():
     name = input("ingresa el nombre del jugador ")
     coins = 2
     influence = []
+    influence.append(influence_deck[0]) #es una manera de darle las dos primeras cartas al jugador ya barajadas
+    influence_deck.pop(0) #quita las cartas entregadas del mazo (influence_deck)
     influence.append(influence_deck[0])
     influence_deck.pop(0)
-    influence.append(influence_deck[0])
-    influence_deck.pop(0)
-    #falta que el jugador reciba dos influencias de influence_deck
+    #falta que el jugador reciba dos influencias de influence_deck 
     players.append(Player(name, influence, coins))
 
-def show_game_status():
-    print("\nJugadores creados:")
+def show_game_status(): #muestra fluencia y monedas de todos los jugadores
+    print("\nSituación Jugadores:")
     for (i, _) in enumerate(players):
-        print(f"{i+1}: {players[i].name} - {players[i].influence} - {players[i].coins}monedas")
+        print(f"{i+1}: {players[i].name} le quedan {len(players[i].influence)} de fluencia y {players[i].coins} monedas")
 
 def print_menu_and_select():
     print ("\nSelecciona una opción:")
     print ("1. Ingresar un jugador")
-    print ("0. Salir")
+    print ("0. Comenzar el juego")
     return int(input())
-    
-def initial_menu():
+
+def print_actions_and_select():
+    print ("\nSeleccione una acción o carta a jugar:")
+    print ("1. Ingresos")
+    print ("2. Ayuda Extranjera")
+    print ("3. Golpe")
+    print ("4. Duque")
+    print ("5. Asesino")
+    print ("6. Capitán")
+    print ("7. Embajador")
+    return int(input())
+
+def show_my_characters():
+    print ("\n¿Desea mirar sus cartas?")
+    print ("1. Si")
+    print ("2. No")
+    return int(input())
+
+def initialize_game():
     while True:
         selection = print_menu_and_select()
         if selection == 0:
             break
         if selection == 1:
             create_player()
-        show_game_status()
+        if len(players) == 4: #si se ingresaron 4 jugadores el juego parte automáticamente
+            break
+
+    while True: #ciclo hasta que termine el juego
+        for i in range(len(players)): #cada jugador realiza una acción por turno
+            show_game_status() #muestra estado actual del juego antes de que comienze el turno de un jugador
+            print ("\n"+"Turno de "+players[i].name)
+            show = show_my_characters() #si el jugador quiere ver sus cartas
+            if show == 1:
+                print(players[i].influence)
+            action = print_actions_and_select()
             
+        if len(players) == 1: #solo queda un jugador, termina el juego
+            break
             
 
-# crear funcion que muestre influencias del jugador en turno y coins del resto
-# crear fx que muestre cada accion
-# crear menu que permita seleccionar acciones y dar la oportunidad a los demas
-# de desafiarla o contraatacarla
+
+# crear funcion que muestre influencias del jugador en turno y coins del resto [LISTO]
+# crear fx que muestre cada accion [LISTO]
+# crear menu que permita seleccionar acciones [LISTO] y dar la oportunidad a los demas
+# de desafiarla o contraatacarla 
 # crear log del turno
+# mejorar calidad en la que se imprimen las cartas de un jugador (que no sea una lista)
+
+
 
 if __name__ == "__main__":
-    initial_menu()
+    initialize_game()
