ecfa347de1fdda79a15b82b581463de8cf8eb76c
AndresILopez <ailopez@miuandes.cl>
2021-04-23 01:36:38 -0400

    dudas con la lógica

--- Character.py
+++ Character.py
@@ -2,6 +2,7 @@
 """
 """
 from main import *
+from numpy import random
 
 class Character:
     def __init__(self, name):
@@ -12,42 +13,59 @@ class Character:
         players[nplayer].coins += 1
         return
     
-    def foreign_aid(nplayer, name_last_influence):
-        # evito hacer una fx de bloqueo en Duke
-        if name_last_influence != "Duke":
-            players[nplayer].coins += 2
-            return
+    def foreign_aid(nplayer):
+        players[nplayer].coins += 2
+        return
         
-    def coup():
+    def coup(nplayer):
+        players[nplayer].coins -= 7
         j = int(input("\nIngresa el número del jugador al cual le aplicarás el golpe "))
-        players[j-1].coins -= 7
+        players[j-1].influence.pop(random.randint(0,1))
         return
     
 class Duke(Character):
     def __init__(self, name):
         Character.__init__(self, name)
     
-    def Tax():
+    def Tax(nplayer):
+        players[nplayer].coins += 3
+        return
+    
+    def Blocks_foreign_aid(nplayer):
+        players[nplayer].coins -= 2
+        return
         
-
 class Captain(Character):
     def __init__(self, name):
         Character.__init__(self, name)
     
     def Steal():
+        j = int(input("\nIngresa el número del jugador al cual extorsionarás "))
+        if players[j-1].coins == 1:
+            players[j-1].coins -= 1
+            return
+        elif players[j-1].coins > 1:
+            players[j-1].coins -= 2
+            return
+        else:
+            return
 
 class Assassin(Character):
     def __init__(self, name):
         Character.__init__(self, name)
     
-    def Assassinate():
+    def Assassinate(nplayer):
+        players[nplayer].coins -= 3
+        j = int(input("\nIngresa el número del jugador al cual le quitarás una influencia "))
+        players[j-1].influence.pop(random.randint(0,1))
+        return
         
-
 class Ambassador(Character):
     def __init__(self, name):
         Character.__init__(self, name)
     
     def Exchange():
+        
         
 
 class contessa(Character):
